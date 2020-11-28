7341e9b8eaf8c8e354a9f3e823000f5796b05147
Daiki Tanaka <daiki.tanaka.g@gmail.com>
2020-11-29 08:56:13 +0900

    Refactor: コントローラの処理をモデルへ移してスリム化しました。

--- app/models/password_encryption.rb
+++ app/models/password_encryption.rb
@@ -1,5 +1,9 @@
 module PasswordEncryption
   def encrypt(text)
+    PasswordEncryption.encrypt(text)
+  end
+
+  def self.encrypt(text)
     text.crypt(PASSWORD_ENCRYPTION_SECRET)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,30 @@ class User < ApplicationRecord
 
   has_many :photos
 
+  scope :authenticated, lambda { |username, password|
+    where(username: username).where(encrypted_password: PasswordEncryption.encrypt(password))
+  }
+
   def password=(new_password)
     self.encrypted_password = encrypt(new_password)
+  end
+
+  def find_my_photo_by(id:)
+    photos.where(id: id).first
+  end
+
+  def self.authenticate(
+    username:,
+    password:,
+    when_succeeded: ->(user) {},
+    when_failed: -> {}
+  )
+    user = authenticated(username, password).first
+    if user.present?
+      when_succeeded.call user
+    else
+      when_failed.call
+    end
   end
 
 end
