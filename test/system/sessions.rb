24562255557283d8a26bb44a0cfc0765306686f3
Daiki Tanaka <daiki.tanaka.g@gmail.com>
2020-11-28 14:38:41 +0900

    Refactor: ユーザーを生成する際に、呼び出し側で暗号化をしなくても済むように変更しました。

--- test/system/sessions.rb
+++ test/system/sessions.rb
@@ -2,10 +2,8 @@ require 'application_system_test_case'
 
 class SessionsCreateTest < ApplicationSystemTestCase
 
-  include PasswordEncryption
-
   test '[正常系]正しいユーザーIDとパスワードを入力するとログインに成功し、写真一覧へリダイレクトされる' do
-    User.create(username: 'user1', encrypted_password: encrypt('password'))
+    User.create(username: 'user1', password: 'password')
     visit new_sessions_path
     fill_in 'forms_session_username', with: 'user1'
     fill_in 'forms_session_password', with: 'password'
@@ -14,7 +12,7 @@ class SessionsCreateTest < ApplicationSystemTestCase
   end
 
   test '[異常系]ユーザーIDとパスワードを入力せずにログインを試みると、ログインできずエラーメッセージが表示される' do
-    User.create(username: 'user1', encrypted_password: encrypt('password'))
+    User.create(username: 'user1', password: 'password')
     visit new_sessions_path
     click_on 'commit'
     assert_text 'ユーザーIDを入力してください'
@@ -22,7 +20,7 @@ class SessionsCreateTest < ApplicationSystemTestCase
   end
 
   test '[異常系]誤ったユーザーIDとパスワードを入力してログインを試みると、ログインできずエラーメッセージが表示される' do
-    User.create(username: 'user1', encrypted_password: encrypt('password'))
+    User.create(username: 'user1', password: 'password')
     visit new_sessions_path
     fill_in 'forms_session_username', with: 'foo'
     fill_in 'forms_session_password', with: 'bar'
@@ -31,7 +29,7 @@ class SessionsCreateTest < ApplicationSystemTestCase
   end
 
   test '[異常系]正しいユーザーIDと、誤ったパスワードを入力してログインを試みると、ログインできずエラーメッセージが表示される' do
-    User.create(username: 'user1', encrypted_password: encrypt('password'))
+    User.create(username: 'user1', password: 'password')
     visit new_sessions_path
     fill_in 'forms_session_username', with: 'user1'
     fill_in 'forms_session_password', with: 'bar'
